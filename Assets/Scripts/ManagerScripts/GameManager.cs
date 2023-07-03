79d5c52ce1e1ea7023164821c682759df97fc672
Janis Butz <janis.butz94@gmail.com>
2023-07-03 14:26:39 +0100

    Holographic shader for map and video, progression check implementation

--- Assets/Scripts/ManagerScripts/GameManager.cs
+++ Assets/Scripts/ManagerScripts/GameManager.cs
@@ -1,32 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class GameManager : MonoBehaviour
 {
-    public bool introductionStart;
-    public bool firstPuzzleDone;
+    public bool startFirstPuzzle, firstPuzzleDone;
     public bool cargoDelivered;
     public bool secondPuzzleDone;
-    
-    // Start is called before the first frame update
+
+
+    public float timer, timeToReach;
+
+    [Header("Start Sequence")] public bool introductionStart;
+    public GameObject introductionHologram;
+    public VideoPlayer introductionVideoPlayer;
+
+
+    [Header("Puzzle 1")] public GameObject puzzle1Hologram;
+
+    // Start is called before the first frame
     void Awake()
     {
-        OVRManager.SetSpaceWarp(true); // Enable space warp
+       // OVRManager.SetSpaceWarp(true); // Enable space warp
+        //introductionVideoPlayer.loopPointReached += EndReached;
+        introductionStart = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (introductionStart)
+        timer += Time.deltaTime;
+
+        if (timer >= timeToReach)
         {
             StartIntroduction();
         }
     }
-    
+
     void StartIntroduction()
     {
-        
+        if (!introductionVideoPlayer.isPlaying && introductionStart)
+        {
+            introductionHologram.SetActive(true);
+            introductionVideoPlayer.Play();
+            introductionVideoPlayer.loopPointReached += EndReached;
+            introductionStart = false;
+        }
     }
-    
+
+
+    void EndReached(VideoPlayer vp)
+    {
+        Debug.Log("Video End Reached");
+        introductionHologram.SetActive(false);
+    }
 }
